444a8dba832460ccd9c12555bb8b0810395149cd
Todd Zimmerman <zimmermant@uwstout.edu>
2017-10-09 12:43:46 -0500

    Added functions.py

---
+++ functions.py
@@ -0,0 +1,15 @@
+# Example of some simple functions
+
+def print_a_string(stuff):
+	# This is a simple function that prints out a string of text
+	print("The string I want printed is ", stuff)
+
+#To call a function, type in the name of the function and pass the required variables.
+
+print_a_string("I am a function")
+
+def cube(x):
+	#Return the cube of a number
+	ans= x**3
+	return ans #The 'return' statement is the result returned by the function
+
